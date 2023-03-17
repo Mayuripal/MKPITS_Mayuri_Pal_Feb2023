75fec7174ec20f117c2ef3a7387d476d2bdb70d4
Mayuripal <mayuri831pal@gmail.com>
2023-03-17 10:36:48 +0530

    dotnet

---
+++ csharp/2 prg/2 prg/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace _2_prg
+{
+    class Program
+    {
+        static void Main()
+        {
+            string name;
+            name = Console.ReadLine();
+            Console.WriteLine("hello mr/miss " + name);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ csharp/helloworld/helloworld/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace helloworld
+{
+     class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("hello from csharp");
+            Console.WriteLine("hello from mkpits");
+            Console.ReadKey();
+
+        }
+    }
+}
