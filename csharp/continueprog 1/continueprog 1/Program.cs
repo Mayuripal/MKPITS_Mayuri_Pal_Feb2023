7757fb8aad58ea3118ec74ce487fa1bc8e14f2db
Mayuripal <mayuri831pal@gmail.com>
2023-03-24 18:28:20 +0530

    update

---
+++ csharp/continueprog 1/continueprog 1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace continueprog_1
+{
+    
+            class Program
+        {
+            static void Main()
+            {
+
+                int i = 1;
+                for (i = 1; i <= 10; i++)
+                {
+                    if (i == 5)
+                    {
+                        break;
+                    }
+                    Console.WriteLine("counter" + i);
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+    }
+}
