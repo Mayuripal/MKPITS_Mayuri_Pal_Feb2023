069a37e521bbb36398538943a164c74301cb1c47
Mayuripal <mayuri831pal@gmail.com>
2023-03-29 18:14:11 +0530

    methods

---
+++ csharp/array intiolization/array intiolization/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace array_intiolization
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //creating a num array of 5 elements
+            int[] num = { 1, 2, 3, 4, 5 };
+
+            //display the values of array
+            for(int row=0; row<num.Length; row++)
+            {
+                Console.WriteLine("num[{0}]={1}", row, num[row]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
