3ba2aba970381c9fd60ea6b784860e48ef85060d
Mayuripal <mayuri831pal@gmail.com>
2023-04-03 14:14:19 +0530

    window forms

---
+++ csharp/assignment2/assignment2/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace assignment2
+{
+    public partial class Form1 : Form
+    {
+        int counter = 0;
+        int []arr=new int[8];
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(counter<arr.Length)
+            {
+                arr[counter] = Convert.ToInt32(textBox3.Text);
+                counter++;
+            }
+
+            else if (counter>arr.Length) 
+            {
+                button1.Enabled = false;
+            }
+            textBox3.Clear();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ csharp/assignment3/assignment3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int [3] ;
+            for (int i=0 ; i<arr.Length ; i++)
+            {
+                Console.WriteLine("enter number :");
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            int temp;
+
+            for(int i=0; i<arr.Length; i++)
+            {
+                for(int j=i+1;j<arr.Length; j++) 
+                {
+                    if (arr[i] == arr[j])
+                    { 
+                        temp = arr[i] + arr[j];
+                    }
+                }
+            }
+
+            for (int i=0; i<arr.Length;i++) 
+            {
+                Console.WriteLine(temp);
+            }
+            Console.ReadLine();
+        }
+    }
+}
