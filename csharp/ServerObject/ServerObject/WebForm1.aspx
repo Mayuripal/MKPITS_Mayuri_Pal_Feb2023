c7049a5f69c59ee07d19c73afbb2ce02e3b7baae
Mayuripal <mayuri831pal@gmail.com>
2023-06-09 18:15:09 +0530

    serverobject method

---
+++ csharp/ServerObject/ServerObject/WebForm1.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="ServerObject.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Webform 1 page<br />
+            <br />
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Execute Method" />
+&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Transfer Method" />
+            <br />
+            <br />
+            <asp:Button ID="Button3" runat="server" OnClick="Button3_Click" Text="Html Encode Server" />
+&nbsp;
+            <asp:Button ID="Button4" runat="server" OnClick="Button4_Click" Text="URL Encode" />
+            <br />
+            <br />
+            <asp:Button ID="Button5" runat="server" OnClick="Button5_Click" Text="URL Method2" />
+&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="Button6" runat="server" OnClick="Button6_Click" Text="URL Method3" />
+            <br />
+            <br />
+            <asp:Button ID="Button7" runat="server" OnClick="Button7_Click" Text="MapPath Method" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ csharp/ServerObject/ServerObject/WebForm1.aspx.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ServerObject
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Response.Write("hello from webform 1");
+            Server.Execute("Webform2.aspx");
+            Response.Write("Welcome back to webform 1");
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            Response.Write("hello from webform 1");
+            Server.Transfer("Webform2.aspx");
+            Response.Write("Welcome back to webform 1");
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            Response.Write(Server.HtmlEncode("<H1> is an example of a Heading tag </ H1 > "));
+
+            //if not written in html encode
+            //Response.Write("<H1> is an example of a Heading tag </ H1 > ");
+
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            Response.Write(Server.UrlEncode("http://localhost / code / webform2.aspx"));
+        }
+
+        protected void Button5_Click(object sender, EventArgs e)
+        {
+            String name = Server.UrlEncode("John Saunders");
+
+            Response.Redirect("webform3.aspx?name=" + name);
+            // name will be shown on address /url bar
+        }
+
+        protected void Button6_Click(object sender, EventArgs e)
+        {
+            String name = Server.UrlEncode("John Saunders");
+
+            String password = Server.UrlEncode("king");
+
+            Response.Redirect("webform3.aspx?name = " + name + " & password = " + password);
+        }
+
+        protected void Button7_Click(object sender, EventArgs e)
+        {
+            Response.Write(Server.MapPath("/webform3.aspx"));
+        }
+    }
+    
+}
