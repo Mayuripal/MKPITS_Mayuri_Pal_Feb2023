6ad2deb44fa80c8652578147e9751b4bd55d7ffc
Mayuripal <mayuri831pal@gmail.com>
2023-06-12 13:15:27 +0530

    gridview in ado

---
+++ csharp/gridviewinado/gridviewinado/WebForm1.aspx
@@ -0,0 +1,64 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="gridviewinado.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 100%;
+        }
+        .auto-style2 {
+            width: 156px;
+        }
+        .auto-style3 {
+            width: 290px;
+        }
+        .auto-style4 {
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="auto-style4">
+            <div class="auto-style4">
+                Grid view in asp.net
+                <br />
+                Total Amount
+                <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+                <br />
+                Paid Amount&nbsp;
+                <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+                <br />
+            </div>
+            <table class="auto-style1">
+                <tr>
+                    <td class="auto-style2">
+                        <br />
+                        <asp:GridView ID="GridView1" runat="server">
+                        </asp:GridView>
+                        <br />
+                    </td>
+                    <td class="auto-style3">
+                        <asp:GridView ID="GridView2" runat="server">
+                        </asp:GridView>
+                    </td>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                    <td class="auto-style2">
+                        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Load" />
+                    </td>
+                    <td class="auto-style3">
+                        <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Show emi" />
+                    </td>
+                    <td>&nbsp;</td>
+                </tr>
+            </table>
+            <br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ csharp/gridviewinado/gridviewinado/WebForm1.aspx.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace gridviewinado
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            DataSet ds = new DataSet();
+            DataTable dt = new DataTable("dtable");
+            DataRow dr;
+
+            dt.Columns.Add(new DataColumn("ID",typeof(int)));
+            dt.Columns.Add(new DataColumn("Name",typeof(string)));
+            dt.Columns.Add(new DataColumn("DOA", typeof(DateTime)));
+
+            dr = dt.NewRow();
+            dr[0] = 1;
+            dr[1] = "pranay";
+            dr[2]=DateTime.Now;
+
+            dt.Rows.Add(dr);
+
+            ds.Tables.Add(dt);
+            GridView1.DataSource = ds.Tables["dtable"].DefaultView;
+            GridView1.DataBind();
+
+            
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            double total = Convert.ToDouble(TextBox1.Text);
+            double paid = Convert.ToDouble(TextBox2.Text);
+            double ramount = total - paid;
+            double eamount =ramount/3 ;
+            DataSet ds1 = new DataSet();
+            DataTable dt1 = new DataTable("emitable");
+            DataRow dr;
+
+            dt1.Columns.Add(new DataColumn("Sr no.",typeof(int)));
+            dt1.Columns.Add(new DataColumn("Product", typeof(string)));
+            dt1.Columns.Add(new DataColumn("Instalments", typeof(double)));
+            for(int i = 1; i <=3; i++)
+            {
+                dr = dt1.NewRow();
+                dr[0] = i;
+                dr[1] = "Shreya";
+                dr[2] = eamount;
+                dt1.Rows.Add(dr);
+            }
+            ds1.Tables.Add(dt1);
+            GridView2.DataSource = ds1.Tables["emitable"].DefaultView;
+            GridView2.DataBind();
+            
+        }
+    }
+}
