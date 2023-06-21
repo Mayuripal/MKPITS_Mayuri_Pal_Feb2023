90b7c586794f7fda8653d16bd1680a42f2134848
Mayuripal <mayuri831pal@gmail.com>
2023-06-21 21:47:53 +0530

    SQL DB

---
+++ SQL/meeshodb.sql
@@ -0,0 +1,6 @@
+create database meeshodb;
+
+----------------------------------------------------------------------
+create table Product(Prodid int identity primary key,Prodname varchar(100),proddescr varchar(100),Catid int,proudprice decimal(10,2),Qty int,Images varchar(100));
+
+select * from Product
