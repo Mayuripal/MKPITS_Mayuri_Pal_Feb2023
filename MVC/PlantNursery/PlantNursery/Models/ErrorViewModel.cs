5d74ac1b3df230b2f2e0325b2b8c40dcb5af7acc
Mayuripal <mayuri831pal@gmail.com>
2023-06-24 22:39:39 +0530

    Mvc webpage

---
+++ MVC/PlantNursery/PlantNursery/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PlantNursery.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
