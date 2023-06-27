f315e1579c98df27abd12fe9866f9e608b06a6bf
Mayuripal <mayuri831pal@gmail.com>
2023-06-27 18:07:02 +0530

    updated webpage

---
+++ MVC/PlantNursery/PlantNursery/Controllers/SignupController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PlantNursery.Models;
+
+namespace PlantNursery.Controllers
+{
+    public class SignupController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Signup(SignUpModel u)
+        {
+            ViewBag.email = u.Email;
+            ViewBag.password = u.Password;
+            return View();
+        }
+    }
+}
---
+++ MVC/PlantNursery/PlantNursery/Models/SignUpModel.cs
@@ -0,0 +1,8 @@
+﻿namespace PlantNursery.Models
+{
+    public class SignUpModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
