fca03330863205ebd8777bbeeb487a2be2d9cb7f
Mayuripal <mayuri831pal@gmail.com>
2023-07-19 21:40:16 +0530

    Mvc web api

---
+++ MVC/EmployeeService/EmployeeService/Controllers/EmployeesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using EmployeeService.Models;
+
+namespace EmployeeService.Controllers
+{
+    public class EmployeesController : ApiController
+    {
+        public IEnumerable<EmployeesController> Get()
+        {
+            using (EmployeeDBContext dBContext=new EmployeeDBContext())
+            {
+                return dBContext.Employees.ToList();
+            }
+        }
+
+        //public EmployeesController Get(int id)
+        //{
+        //    using (EmployeeDBContext dBContext=new EmployeeDBContext())
+        //    {
+        //        return dBContext.Employees.FirstOrDefault(e => e.ID == id);
+        //    }
+        //}
+    }
+}
---
+++ SQL/WEBAPI.sql
@@ -0,0 +1,23 @@
+CREATE DATABASE WEBAPI_DB;
+----------------------------------------------------------------------------
+CREATE TABLE Employees
+(
+     ID int primary key identity,
+     FirstName nvarchar(50),
+     LastName nvarchar(50),
+     Gender nvarchar(50),
+     Salary int
+);
+-----------------------------------------------------------------------------
+INSERT INTO Employees VALUES ('Pranaya', 'Rout', 'Male', 60000)
+INSERT INTO Employees VALUES ('Anurag', 'Mohanty', 'Male', 45000)
+INSERT INTO Employees VALUES ('Preety', 'Tiwari', 'Female', 45000)
+INSERT INTO Employees VALUES ('Sambit', 'Mohanty', 'Male', 70000)
+INSERT INTO Employees VALUES ('Shushanta', 'Jena', 'Male', 45000)
+INSERT INTO Employees VALUES ('Priyanka', 'Dewangan', 'Female', 30000)
+INSERT INTO Employees VALUES ('Sandeep', 'Kiran', 'Male', 45000)
+INSERT INTO Employees VALUES('Shudhansshu', 'Nayak', 'Male', 30000)
+INSERT INTO Employees VALUES ('Hina', 'Sharma', 'Female', 35000)
+INSERT INTO Employees VALUES ('Preetiranjan', 'Sahoo', 'Male', 80000)
+------------------------------------------------------------------------------
+SELECT * FROM Employees
